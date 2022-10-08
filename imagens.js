bba738a5288589061044b0cc2c70ecadbc3dad8c
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2022-10-08 17:37:40 -0300

    corrigindo nome do autor

--- imagens.js
+++ imagens.js
@@ -7,7 +7,7 @@ let imagemCarro3;
 
 
 function preload(){ imagemEstrada=loadImage("imagens/estrada.png");
-imagemAtor=loadImage("imagens/ator-1.png");
+imagemAtor=loadImage("imagens/personagem.png");
 imagemCarro=loadImage("imagens/carro-1.png");
 imagemCarro2=loadImage("imagens/carro-2.png");
 imagemCarro3=loadImage("imagens/carro-3.png");
