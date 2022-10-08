3daac62bb6c33d314247c52ef0d732a0e73cffac
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2022-10-08 17:35:47 -0300

    corrigindo nomes das pastas

--- imagens.js
+++ imagens.js
@@ -6,12 +6,12 @@ let imagemCarro2;
 let imagemCarro3;
 
 
-function preload(){ imagemEstrada=loadImage("Imagens/estrada.png");
-imagemAtor=loadImage("Imagens/ator-1.png");
-imagemCarro=loadImage("Imagens/carro-1.png");
-imagemCarro2=loadImage("Imagens/carro-2.png");
-imagemCarro3=loadImage("Imagens/carro-3.png");
+function preload(){ imagemEstrada=loadImage("imagens/estrada.png");
+imagemAtor=loadImage("imagens/ator-1.png");
+imagemCarro=loadImage("imagens/carro-1.png");
+imagemCarro2=loadImage("imagens/carro-2.png");
+imagemCarro3=loadImage("imagens/carro-3.png");
 imagemCarros= [imagemCarro,imagemCarro2,imagemCarro3,imagemCarro,imagemCarro3,imagemCarro2];
-sonsDaTrilha = loadSound("Sons/trilha.mp3");
-somDaColisao = loadSound( "Sons/colidiu.mp3");
-somPontos = loadSound ( "Sons/pontos.wav");}
+sonsDaTrilha = loadSound("sons/trilha.mp3");
+somDaColisao = loadSound( "sons/colidiu.mp3");
+somPontos = loadSound ( "sons/pontos.wav");}
